9c170d38bc2bb47b9406c9fe5f3f7b527bb2a7d9
Shivam1504 <shivamji019@gmail.com>
2023-11-28 15:03:01 +0530

    day_5

---
+++ Day5_React/my-app/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const App = () => {
+
+  const name ='shivam';
+  const name2 ='mohan';
+  return (
+    <>
+    <div>
+
+<h1>My nme is {name}</h1>
+<p>my name is {name2}</p>
+
+    </div>
+
+    <div>
+      <h1>hello n</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
